5ea8142e0d6c5a93c8753109677a236148b5a15f
Abhishek <abhishekparmar162@gmail.com>
2025-07-23 13:12:08 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 //cors
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
